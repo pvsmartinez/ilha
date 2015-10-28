bea8934dbdc05ee117b4d4c3a24dba0b55f6a0ef
dvdwn <davidwen.wen@gmail.com>
2015-10-28 19:53:21 -0200

    Removed sea glow

--- Source/Sea.hx
+++ Source/Sea.hx
@@ -1,12 +1,6 @@
 package ;
 
-import openfl.filters.BitmapFilter;
-import openfl.filters.GlowFilter;
-import openfl.filters.BitmapFilterQuality;
-
 class Sea extends Body {
-
-	private var shine:GlowFilter;
 
 	public function new() {
 		super('sea', 800, 100);
